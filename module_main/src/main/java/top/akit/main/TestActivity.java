cf06aaa62fe13ce26c9117097e807d71b504e4f5
胡昌鹏 <roc.hu@msn.com>
2017-11-17 14:18:35 +0800

    * v0.0.1:动态配置编译信息加入app

--- module_main/src/main/java/top/akit/main/TestActivity.java
+++ module_main/src/main/java/top/akit/main/TestActivity.java
@@ -1,6 +1,7 @@
 package top.akit.main;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import top.akit.base.BaseActivity;
@@ -13,5 +14,17 @@ public class TestActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_test);
         setTitle(R.string.main_app_name);
+
+        TextView tv = findViewById(R.id.tv);
+
+        String buildTime = getResources().getString(R.string.build_time);
+        String buildHost = getResources().getString(R.string.build_host);
+        String buildRevision = getResources().getString(R.string.build_revision);
+
+        StringBuilder tvBuilder = new StringBuilder();
+        tvBuilder.append("buildTime:").append(buildTime).append("\n");
+        tvBuilder.append("buildHost:").append(buildHost).append("\n");
+        tvBuilder.append("buildRevision:").append(buildRevision).append("\n");
+        tv.setText(tvBuilder);
     }
 }
